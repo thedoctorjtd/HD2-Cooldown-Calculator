1e41ff2805393c5a2fe86b52e0aeeaf6bfaa736b
thedoctorjtd <76110609+thedoctorjtd@users.noreply.github.com>
2025-08-29 01:50:05 -0400

    fixed npm error page not showing 404 text correctly.

--- svelte/src/routes/+error.svelte
+++ svelte/src/routes/+error.svelte
@@ -2,7 +2,9 @@
   import ErrorPage from '$lib/components/ErrorPage.svelte';
   export let status: number = 404;
   export let error: unknown;
-  const detail = (error as any)?.message || 'Unexpected error.';
+  const detail = status === 404
+    ? 'The page you requested could not be located.'
+    : ((error as any)?.message ?? 'Unexpected error.');
 </script>
 
 <ErrorPage code={status} detail={detail} />
